cd814e7548999cb0a770ef9c99d34c0c766b27f8
Thumaes <118362290+Thumaes@users.noreply.github.com>
2023-12-31 22:31:23 +0000

    yoo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BBQ Recepten</title>
+    <!-- Voeg hier de stijl toe volgens de ontwerpeisen -->
+</head>
+<body>
+    <h1>BBQ Recepten</h1>
+    
+    <!-- Toon hier uitgelichte recepten -->
+    
+    <!-- Voeg navigatie toe naar receptenpagina -->
+    <a href="recepten.php">Bekijk alle recepten</a>
+</body>
+</html>
