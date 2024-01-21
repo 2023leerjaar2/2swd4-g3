f2ce456ed5c15bf609dab82291562cd8048cc9c2
Hase-GH <71468692+Hase-GH@users.noreply.github.com>
2024-01-22 00:57:31 +0100

    styling

--- login.php
+++ login.php
@@ -36,8 +36,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
 <header>
-        <h1>Kamadoing</h1>
+        
         <nav>
+            <img class="logo" src="https://cdn.discordapp.com/attachments/581190740479311893/1198765338460962836/Asset_2.png?ex=65c01838&is=65ada338&hm=b3b142e452fc2c2b58df27d2a781bf6303e7911b8abaa6c58603a73084cbd125&" alt="">
+            <div>
             <a href="index.php">Home</a>
             <?php
             if (isset($_SESSION['user'])) {
@@ -48,6 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
             ?>
             <a href="contact.php">Contact</a>
+            </div>
         </nav>
     </header>
     <div class="login-container">
